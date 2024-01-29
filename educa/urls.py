4cf9e8b2784263fe07eb575ff71d50ead2c0aa24
Yuzyzy88 <ayu.sulistiowati@protonmail.com>
2024-01-29 14:06:31 +0700

    Building an API

--- educa/urls.py
+++ educa/urls.py
@@ -26,8 +26,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('course/', include('courses.urls')),
     path('students/', include('students.urls')),
+    path('api/', include('courses.api.urls', namespace='api')),
     path('', CourseListView.as_view(), name='course_list'),
     path('__debug__/', include('debug_toolbar.urls')),
+
 ]
 
 if settings.DEBUG:
